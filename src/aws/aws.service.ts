2b1b3d345613379688f3390788e301de2d998694
jj2084jj <whdms3368@gmail.com>
2024-11-03 19:03:38 +0900

    빌드 오류 수정

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -1,38 +1,35 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
-import { v4 as uuidv4 } from 'uuid';
-import * as AWS from 'aws-sdk';
 
 @Injectable()
 export class AwsService {
-  private readonly s3: AWS.S3;
+  private readonly s3Client: S3Client;
 
   constructor() {
-    this.s3 = new AWS.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    this.s3Client = new S3Client({
       region: process.env.AWS_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
     });
   }
 
   async uploadFile(file: Express.Multer.File) {
-    // 파일 확장자 추출
-    const fileExt = file.originalname.split('.').pop();
-    // UUID로 유니크한 파일명 생성
-    const key = `${uuidv4()}.${fileExt}`;
-
-    const params = {
+    const key = `images/${Date.now()}_${file.originalname}`;
+    
+    const command = new PutObjectCommand({
       Bucket: process.env.AWS_S3_BUCKET_NAME,
       Key: key,
       Body: file.buffer,
       ContentType: file.mimetype,
-    };
+    });
 
     try {
-      const result = await this.s3.upload(params).promise();
+      await this.s3Client.send(command);
       return {
-        key: result.Key,
-        url: result.Location,
-        success: true
+        url: `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`,
+        key: key,
       };
     } catch (error) {
       throw new Error(`Failed to upload file to S3: ${error.message}`);
